c6d7a09bb4791e1ae344cae52b62c01024dcafb7
Shashank Sahni <shredder12@gmail.com>
2014-07-18 06:39:32 +0530

    Added options to teagent public cookbook
    * repo_setup, agent_utils, proxy_{user,pass} attributes added to
    cookbook.
    * Version bump to 0.0.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'opensource+chef@thousandeyes.com'
 license          'GPLv3'
 description      'Installs and configures the ThousandEyes private agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.0.2'
 provides         'teagent'
 
 recipe 'teagent', 'Installs and configures the ThousandEyes private agent'
