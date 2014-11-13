8a410eb7f3de9e8abdea9d6ca39bd211a6916709
Dave Fraleigh <dave@thousandeyes.com>
2014-11-12 16:55:13 -0800

    Updated nomenclature (Private -> Enterprise)

--- metadata.rb
+++ metadata.rb
@@ -2,12 +2,12 @@ name             'teagent'
 maintainer       'ThousandEyes'
 maintainer_email 'opensource+chef@thousandeyes.com'
 license          'GPLv3'
-description      'Installs and configures the ThousandEyes private agent'
+description      'Installs and configures the ThousandEyes Enterprise Agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.2'
 provides         'teagent'
 
-recipe 'teagent', 'Installs and configures the ThousandEyes private agent'
+recipe 'teagent', 'Installs and configures the ThousandEyes Enterprise Agent'
 
 # actually tested on
 supports 'redhat'
