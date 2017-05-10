94753566db77e5e633ac2429258747fea44e2aeb
Gaston Bezzi <gaston@gb.local>
2017-05-10 14:35:28 +0100

    OPS-3192: Added Xenial support.

--- recipes/service.rb
+++ recipes/service.rb
@@ -8,9 +8,21 @@
 
 service 'te-agent' do
     case node['platform']
-    when 'redhat','centos','ubuntu'
+    when 'redhat','centos'
         provider Chef::Provider::Service::Upstart
         supports [:restart, :status]
+
+    when 'ubuntu'
+        case node['platform_version']
+        when '16.04'
+            provider Chef::Provider::Service::Systemd
+            supports [:restart, :status]
+        else
+            provider Chef::Provider::Service::Upstart
+            supports [:restart, :status]
+        end
+
+
     when 'debian'
         pattern 'te-agent'
         supports [:restart]
@@ -18,4 +30,3 @@ service 'te-agent' do
     #supports [:restart, :status]
     action [:start, :enable]
 end
-
