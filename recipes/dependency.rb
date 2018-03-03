5677c34e0fee57d80286a74f5735701a4d427803
Scott Nelson Windels <swindels@slack-corp.com>
2018-03-02 16:14:32 -0800

    FC001 fixes

--- recipes/dependency.rb
+++ recipes/dependency.rb
@@ -8,7 +8,7 @@
 case node['platform']
 
 when 'ubuntu'
-    case node[:platform_version]
+    case node['platform_version']
     when '14.04' , '16.04'
         package 'lsb-release' do
             action :install
@@ -18,7 +18,7 @@ when 'ubuntu'
         Chef::Application.fatal!('Only Ubuntu 14.04(trusty) and 16.04 (xenial) are supported. Please contact support.')
     end
 when 'rhel','redhat','centos'
-    if node[:platform_version].to_f >= 6.3
+    if node['platform_version'].to_f >= 6.3
         include_recipe 'teagent::repository'
     else
         Chef::Application.fatal!("Please upgrade your operating system (#{node['platform']}) to >= 6.3")
