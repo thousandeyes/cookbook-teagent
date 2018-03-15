6da11ab35aadc566c420916d98d7f328b3dbb1a3
Scott Nelson Windels <swindels@slack-corp.com>
2018-03-15 15:23:43 -0700

    cookstyle fixes for service.rb

--- recipes/service.rb
+++ recipes/service.rb
@@ -7,24 +7,21 @@
 #
 
 service 'te-agent' do
-    case node['platform']
-        when 'redhat','centos'
-            if node['platform_version'].to_f < 7.0
-                provider Chef::Provider::Service::Upstart
-                supports [:restart, :status]
-            else
-                provider Chef::Provider::Service::Systemd
-                supports [:restart, :status]
-            end
-
-        when 'ubuntu'
-            if node['platform_version'].to_f < 15.04
-                provider Chef::Provider::Service::Upstart
-                supports [:restart, :status]
-            else
-                provider Chef::Provider::Service::Systemd
-                supports [:restart, :status]
-            end
+  case node['platform']
+  when 'redhat', 'centos'
+    if node['platform_version'].to_f < 7.0
+      provider Chef::Provider::Service::Upstart
+    else
+      provider Chef::Provider::Service::Systemd
     end
-    action [:start, :enable]
+
+  when 'ubuntu'
+    if node['platform_version'].to_f < 15.04
+      provider Chef::Provider::Service::Upstart
+    else
+      provider Chef::Provider::Service::Systemd
+    end
+  end
+  supports [:restart, :status]
+  action [:start, :enable]
 end
