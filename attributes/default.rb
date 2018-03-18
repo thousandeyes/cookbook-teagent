19abecc05ced4072128367ca08d45c1de29e779f
Scott Nelson Windels <swindels@slack-corp.com>
2018-03-17 18:46:12 -0700

    Remove unused attributes after remove shell script

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,8 +11,6 @@ default['teagent']['ip_version'] = 'ipv4'
 default['teagent']['interface'] = ''
 default['teagent']['agent_utils'] = false
 default['teagent']['set_repo'] = true
-# Set this attribute true if you want to use the template config instead of the shell script config
-default['teagent']['chef_template_config'] = false
 
 # Configuration settings
 default['teagent']['config']['account_token'] = '<account-token>'
@@ -25,7 +23,3 @@ default['teagent']['config']['proxy_location'] = ''
 default['teagent']['config']['proxy_user'] = ''
 default['teagent']['config']['proxy_pass'] = ''
 default['teagent']['config']['crash_reports'] = 1
-
-# Obsolete configuration settings still used by config_teagent.sh
-default['teagent']['config']['proxy_host'] = ''
-default['teagent']['config']['proxy_port'] = ''
